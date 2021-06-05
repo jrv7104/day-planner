79f399fea4c7dd912a04e010c1f361d68a723aea
Jason Viox <jrv04@hotmail.com>
2021-06-05 09:40:19 -0500

    First commit

---
+++ Develop/script.js
@@ -0,0 +1,18 @@
+//Display current day
+
+var day = [];
+
+//Display hourly blocks
+var hour = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
+
+//Change color of hour block based off of if hour is current, the past or the future (see CSS file)
+//var present = ;
+//var past = ;
+//var future = ;//
+
+//Add ability to edit events in every hour block
+
+//Save events where they will still be present when page refreshes
+
+
+moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
